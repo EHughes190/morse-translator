9e94e731351c8cc1728f646dd71a9d1483548e83
EddieSingh <77494320+EddieSingh@users.noreply.github.com>
2021-05-25 14:26:34 +0100

    Working on dyanmic text

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-import { convertToMorse, convertToEnglish } from "./functions.js";
+import { convertToMorse, convertToEnglish, toggleBool } from "./functions.js";
 
 const input = document.getElementById("english-input");
 const output = document.querySelector(".main-content__result");
 const translateBtn = document.querySelector(".translate");
 const switchBtn = document.querySelector(".switch");
 const subHeading = document.querySelector(".heading-subtitle");
+const heading = document.querySelector(".heading");
 let isCalculatingMorse = true;
 
 translateBtn.addEventListener("click", () => {
@@ -20,16 +21,15 @@ translateBtn.addEventListener("click", () => {
 switchBtn.addEventListener("click", () => {
   output.innerHTML = "";
   input.value = "";
+  const info = document.createElement("p");
 
-  toggleBool(isCalculatingMorse);
-  console.log(isCalculatingMorse);
+  isCalculatingMorse = toggleBool(isCalculatingMorse);
   if (!isCalculatingMorse) {
     subHeading.innerHTML = "Morse Code to English";
+    info.innerHTML =
+      "Place a space between each letter, and a slash between words";
   } else {
     subHeading.innerHTML = "English to Morse Code";
+    info.innerHTML = "";
   }
 });
-
-const toggleBool = () => {
-  isCalculatingMorse = !isCalculatingMorse;
-};
