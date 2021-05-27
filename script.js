d970a5e1e2be3e9cb1ce73a51f42ce48c97f1848
EddieSingh <77494320+EddieSingh@users.noreply.github.com>
2021-05-27 10:41:28 +0100

    Updated responsiveness for laptop

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const input = document.getElementById("english-input");
 const output = document.querySelector(".main-content__result");
 const translateBtn = document.querySelector(".translate");
 const switchBtn = document.querySelector(".switch");
-const subHeading = document.querySelector(".heading-subtitle");
+const subHeading = document.querySelector(".heading__subtitle");
 const heading = document.querySelector(".heading");
 let isCalculatingMorse = true;
 
@@ -21,15 +21,11 @@ translateBtn.addEventListener("click", () => {
 switchBtn.addEventListener("click", () => {
   output.innerHTML = "";
   input.value = "";
-  const info = document.createElement("p");
 
   isCalculatingMorse = toggleBool(isCalculatingMorse);
   if (!isCalculatingMorse) {
     subHeading.innerHTML = "Morse Code to English";
-    info.innerHTML =
-      "Place a space between each letter, and a slash between words";
   } else {
     subHeading.innerHTML = "English to Morse Code";
-    info.innerHTML = "";
   }
 });
